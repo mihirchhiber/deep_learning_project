e3b04debd1bf07eceed6884f95bd42d51a947026
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-04-19 13:49:31 +0800

    Update custom cnn

--- src/models/CustomCNN.py
+++ src/models/CustomCNN.py
@@ -2,45 +2,41 @@ import torch
 import torch.nn as nn
 
 class CustomCNN(nn.Module):
-  def __init__(self):
-    super().__init__()
+    def __init__(self, num_filters=(16, 32, 64, 128)):
+        super().__init__()
+        self.num_filters = num_filters
+        fl1, fl2, fl3, fl4 = self.num_filters
 
-    self._convblocks = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d((3,3), (2,2)),
+        self.embed = nn.Sequential(
+            self.conv3x3(1, fl1),
+            nn.MaxPool2d((3, 3), (2, 2)),
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d((2,3), (2,2)),
+            self.conv3x3(fl1, fl2),
+            nn.MaxPool2d((2, 3), (2, 2)),
 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d((5,6), (5,6)),
+            self.conv3x3(fl2, fl3),
+            nn.MaxPool2d((5, 6), (5, 6)),
 
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            self.conv3x3(fl3, fl4),
+            nn.MaxPool2d((6, 6), (5, 4)),
+
+            nn.Flatten(),
+            nn.Linear(6*fl4, 4*fl4),
             nn.ReLU(),
-            nn.MaxPool2d((6,6), (5,4))
+            nn.Dropout(0.2)
         )
-    self._classifier = nn.Sequential(nn.Linear(in_features=1536, out_features=1024),
-                                      nn.ReLU(),
-                                      nn.Dropout(),
-                                      nn.Linear(in_features=1024, out_features=10))
-    # self._prob = nn.Softmax(dim=1)
-    self.apply(self._init_weights)
 
-  def forward(self, x):
-      x = self._convblocks(x)
-      x = x.view(x.size(0), -1)
-      # out = self._classifier(x)
-      # score = self._prob(out)
-      score = self._classifier(x)
-      return score
+        self.classify = nn.Linear(4*fl4, 10)
 
-  def _init_weights(self, layer) -> None:
-      if isinstance(layer, nn.Linear):
-        nn.init.xavier_uniform_(layer.weight)
+    def conv3x3(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels,
+                      kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, input):
+        embedding = self.embed(input)
+        output = self.classify(embedding)
+        return output
