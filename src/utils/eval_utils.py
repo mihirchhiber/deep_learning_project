e92484cdec4cb78702e7f3fe2e9b5e8e51b25e63
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-04-22 00:02:24 +0800

    Minor correction to evaluation

--- src/utils/eval_utils.py
+++ src/utils/eval_utils.py
@@ -8,7 +8,7 @@ def eval_model(model, dataloaders, configs):
 
     t_output = []
     t_pred = []
-    y_test = []
+    y_actual = []
     top_k = []
     # Iterate over data.
     i = 1
@@ -21,7 +21,7 @@ def eval_model(model, dataloaders, configs):
             inputs = inputs.to(device=configs.device)
 
         labels = labels.to(device=configs.device)
-        y_test.append(labels)
+        y_actual.append(labels)
 
         # track history if only in train
         with torch.no_grad():
@@ -32,14 +32,14 @@ def eval_model(model, dataloaders, configs):
             _, temp2 = outputs.topk(5)
             top_k.append(temp2)
 
-    y_test = torch.cat(y_test).cpu().detach().numpy() 
-    y_test_num = torch.cat(t_pred).cpu().detach().numpy() 
+    y_actual = torch.cat(y_actual).cpu().detach().numpy() 
+    y_test = torch.cat(t_pred).cpu().detach().numpy() 
     # y_pred = torch.cat(top_k).cpu().detach().numpy() 
 
     print('\nConfusion Matrix')
-    conf_mt = confusion_matrix(y_test_num, y_test)
+    conf_mt = confusion_matrix(y_actual, y_test)
     print(conf_mt)
     plt.matshow(conf_mt)
     plt.show()
     print('\nClassification Report')
-    print(classification_report(y_test_num, y_test, zero_division=0))
+    print(classification_report(y_actual, y_test, zero_division=0))
