7b4cd727013232a93027cc650cddad0aeae5fc75
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-04-22 22:32:56 +0800

    Modify cnn for updated dataset

--- src/models/CustomCNN.py
+++ src/models/CustomCNN.py
@@ -1,32 +1,34 @@
 import torch
 import torch.nn as nn
 
-class CustomCNN(nn.Module):
-    def __init__(self, configs, num_filters=(16, 32, 64, 128)):
-        super().__init__()
-        self.num_filters = num_filters
-        fl1, fl2, fl3, fl4 = self.num_filters
 
+class CustomCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
         self.embed = nn.Sequential(
-            self.conv3x3(1, fl1),
+            self.conv3x3(1, 16),
             nn.MaxPool2d((3, 3), (2, 2)),
 
-            self.conv3x3(fl1, fl2),
+            self.conv3x3(16, 32),
             nn.MaxPool2d((2, 3), (2, 2)),
 
-            self.conv3x3(fl2, fl3),
+            self.conv3x3(32, 64),
             nn.MaxPool2d((5, 6), (5, 6)),
 
-            self.conv3x3(fl3, fl4),
+            self.conv3x3(64, 128),
             nn.MaxPool2d((6, 6), (5, 4)),
 
             nn.Flatten(),
-            nn.Linear(6*fl4, 4*fl4),
-            nn.ReLU(),
-            nn.Dropout(0.2)
+            nn.Linear(2*128, 128),
+            nn.ReLU()
         )
 
-        self.classify = nn.Linear(4*fl4, 10)
+        self.classify = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(128, 10)
+        )
+
+        self.apply(self._init_weights)
 
     def conv3x3(self, in_channels, out_channels):
         return nn.Sequential(
@@ -40,3 +42,7 @@ class CustomCNN(nn.Module):
         embedding = self.embed(input)
         output = self.classify(embedding)
         return output
+
+    def _init_weights(self, layer):
+        if isinstance(layer, (nn.Linear, nn.Conv2d)):
+            nn.init.kaiming_normal_(layer.weight, nonlinearity='relu')
